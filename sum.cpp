812c52486a57081a17443bfadd785fd06d9d8bb9
Kumarshailesh9 <128351874+Kumarshailesh9@users.noreply.github.com>
2023-09-23 01:34:13 +0530

    comments

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,15 @@
+//preprocessor
 #include<iostream>
 using namespace std;
+
+//main 
 int main()
 {
+  // frist element
 int a=5;
+  //second element
 int b= 10;
+  //add both
 int c= a+b;
 cout<<c;
 }
